09a1444050ec67fecc03c3d76196dd7a0601508c
greek-n-chic <moisovadiana@gmail.com>
2025-06-16 19:54:34 +0300

    Fix files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { getName, greeting } from "../src/cli.js";
+import { getName, greeting } from '../src/cli.js'
 
-console.log('Welcome to the Brain-Games!');
+console.log('Welcome to the Brain-Games!')
 const name = getName()
-console.log(greeting(name));
+console.log(greeting(name))
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export const getName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  return userName;
-};
+  const userName = readlineSync.question('May I have your name? ')
+  return userName
+}
 
 export const greeting = (name) => {
-  return `Hello, ${name}!`;
-};
+  return `Hello, ${name}!`
+}
