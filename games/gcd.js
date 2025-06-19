d448aeb8a5ab73763eea43a92e6b524d15e785b6
greek-n-chic <moisovadiana@gmail.com>
2025-06-19 13:48:04 +0300

    Add brain-gcd game

---
+++ games/gcd.js
@@ -0,0 +1,27 @@
+import { randomNumber } from '../src/cli.js'
+import { greetUser, game } from '../src/common.js'
+
+const rules = 'Find the greatest common divisor of given numbers.'
+greetUser(rules)
+
+const getGcd = (a, b) => {
+  let num1 = a
+  let num2 = b
+  while (num2 !== 0) {
+    const temp = num1 % num2
+    num1 = num2
+    num2 = temp
+  }
+  return num1
+}
+
+export const gcd = () => {
+  const getQuestionAndAnswer = () => {
+    const a = randomNumber(1, 10)
+    const b = randomNumber(1, 10)
+    const question = `${a} ${b}`
+    const correctAnswer = String(getGcd(a, b))
+    return { question, correctAnswer }
+  }
+  game(getQuestionAndAnswer)
+}
